cfec7b076e144075c531dbd5aa742d7461508b8a
AnthonyZet <zejda.anthony@gmail.com>
2023-03-06 19:43:37 +0100

    final console og

--- src/pages/api/again.ts
+++ src/pages/api/again.ts
@@ -15,54 +15,48 @@ async function Page({ finalCurrencies }: { finalCurrencies: Currency[] }):any {
   return final;
 }
 
-// This gets called on every request
+
  async function getServerSideProps() {
   // Fetch data from external API
   const res = await fetch(
     "http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt"
   );
   const data = await res.text();
+  
+      return data
+  }
+
+  
   const finalCurrencies: Currency[] = [];
 
- 
+  const zpracujData = (data:any)=>{
+    const currenciesArray = data.split("\n");
+    const currenciesArray2 = currenciesArray.slice(2, -1);
+    const currenciesArray3 = currenciesArray2.map((currency) => {
+      const currencyArray = currency.split("|");
 
-
- async function awaitData(){
-  const dataCNB = await data;
+      //console.log(currencyArray);
   
-  const currenciesArray = dataCNB.split("\n");
-  const currenciesArray2 = currenciesArray.slice(2, -1);
-  const currenciesArray3 = currenciesArray2.map((currency) => {
-    const currencyArray = currency.split("|");
-
-
-    return finalCurrencies.push({
-      country: currencyArray[0],
-      currencyName: currencyArray[1],
-      code: currencyArray[3],
-      amount: currencyArray[4],
-      currency: currencyArray[2],
+      return finalCurrencies.push({
+        country: currencyArray[0],
+        currencyName: currencyArray[1],
+        code: currencyArray[3],
+        amount: currencyArray[4],
+        currency: currencyArray[2],
+      });
     });
-  });
- }
- awaitData()
+  }
 
- async function logThis(){
-  const dat = await awaitData();
-  console.log('-------------------------')
-  return console.log(finalCurrencies);
-
- }
- logThis()
-      // Pass data to the page via props
-
-  // -----------
-  //Jak tohle dostat do Page ?????
-  // -----------
-   // return { props: { finalCurrencies } };   <---------------
-//-----------------------------------------
-
-
+// This should send props to PAGE component WHY DOESN'T IT WORK?
+const runIt = async()=>{
+  const data = await getServerSideProps();
+  zpracujData(data);
+  console.log(finalCurrencies);
+  
+ return {props: {finalCurrencies: zpracujData(data)}}
 }
-getServerSideProps()
+
+runIt()
+
+
 export default Page;
