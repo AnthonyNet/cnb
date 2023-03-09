2d40d56d10390b5570422b65d328e9a5b11bd726
AnthonyZet <zejda.anthony@gmail.com>
2023-03-09 20:06:29 +0100

    konzultace

--- src/pages/api/again.ts
+++ src/pages/api/again.ts
@@ -8,57 +8,31 @@ interface Currency {
   currency: string;
 }
 
-async function Page({ finalCurrencies }: { finalCurrencies: Currency[] }):any {
-    const final = await finalCurrencies;
+async function Page({ saveData }: { saveData: Currency[] }):any {
+    const final = await saveData;
     console.log(final);
     
   return final;
 }
 
 
- async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch(
-    "http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt"
-  );
-  const data = await res.text();
   
-      return data
-  }
-
-  
-  const finalCurrencies: Currency[] = [];
-
-  const zpracujData = (data:string)=>{
-    const currenciesArray = data.split("\n");
-    const currenciesArray2 = currenciesArray.slice(2, -1);
-    const currenciesArray3 = currenciesArray2.map((currency) => {
-      const currencyArray = currency.split("|");
-
-      //console.log(currencyArray);
-  
-      return finalCurrencies.push({
-        country: currencyArray[0],
-        currencyName: currencyArray[1],
-        code: currencyArray[3],
-        amount: currencyArray[4],
-        currency: currencyArray[2],
-      });
-    });
-  }
-
 // This should send props to PAGE component WHY DOESN'T IT WORK?
 const runIt = async()=>{
   const data = await getServerSideProps();
   const saveData = zpracujData(data);
   
   
-  console.log(finalCurrencies);
+ // console.log(finalCurrencies);
   
- return {props: {finalCurrencies: saveData}}
+ return {props: {saveData}}
 }
 
 runIt() 
+
+
+
+
 
 
 
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,12 +1,48 @@
 import Page from './api/again';
 
-function Main () {
+interface Currency {
+    country: string;
+    currencyName: string;
+    code: string;
+    amount: string;
+    currency: string;
+  }
+
+const zpracujData = (data:string)=>{
+
+    return data.split('\n').slice(2,-1).map((row)=>{
+        const [country,currencyName, currency, code, amount ] = row.split('|');
+        return {
+            country ,
+            currencyName,
+            code,
+            amount, 
+            currency,
+        }
+    })
+  }
+
+
+export async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await fetch(
+      "http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt"
+    );
+    const rawData = await res.text();
+    const data = zpracujData(rawData);
+    
+        return {props:{data}}
+    }
+
+
+function Main ({data}:{data:Currency[]}) {
+console.log(data);
 
     return(
      
         <div>
             <h1> Main </h1>
-            <Page />
+           <pre>{JSON.stringify(data)}</pre>
             <p> Main page </p>
         </div>
 
