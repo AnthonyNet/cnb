594703245fd7a91b873fdefa918f5766c66fa40b
AnthonyNet <zejda.antonin@gmail.com>
2023-04-30 17:39:18 +0200

    doesnt work - error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 
 import { Inter } from 'next/font/google'
 
-import Main from './main'
+import Main from './Main'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
   return (
     <section className='w-screen h-screen'>
      <h2>df</h2>
-    
+
       <Main />
     </section>
   )
